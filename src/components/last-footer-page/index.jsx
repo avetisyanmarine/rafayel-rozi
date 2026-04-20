3763084c762a998668018d65832b082bda53b762
avetisyanmarine <marineavet2006@gmail.com>
2026-04-20 22:17:47 +0400

    qwe

--- src/components/last-footer-page/index.jsx
+++ src/components/last-footer-page/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "../../GlobalStyle";
 import { LastFooterPagePart, LastFooterPagePartContext } from "./styled";
 import PhoneSvg from "../../assets/vectors/Phone.svg";
-// import Instagram from "../../assets/vectors/instagram.svg";
+import Instagram from "../../assets/vectors/instagram.svg";
 
 export const LastFooterPage = () => {
   return (
@@ -12,7 +12,7 @@ export const LastFooterPage = () => {
             <p>Պատրաստվել է</p>
             <div className="flex items-center gap-1 mx-1">
               <img src={Instagram} alt="Instagram" className="inline-block" />
-              {/* <a href="https://www.instagram.com/webinvite.am/"> WEB INVITE </a> */}
+              <a href="https://www.instagram.com/webinvite.am/"> WEB INVITE </a>
             </div>
             <p>կողմից</p>
           </div>
